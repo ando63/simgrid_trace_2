262d5e37507ec65504510ba85d9061d484c24053
ando63 <126371445+ando63@users.noreply.github.com>
2024-03-11 00:21:11 +0900

    Update xmlgen_crossbar_cs.py

--- NPB3.3-MPI/xmlgen_crossbar_cs.py
+++ NPB3.3-MPI/xmlgen_crossbar_cs.py
@@ -151,6 +151,7 @@ if __name__ == "__main__":
 
         for r1, r2 in edgelist_cs:
             if (r1-1, r2-1) not in edgelist:
-                f.write(route_router_router_cs.format(r1,r2))
+                if (r2-1, r1-1) not in edgelist:
+                    f.write(route_router_router_cs.format(r1,r2))
             
         f.write(footer)
