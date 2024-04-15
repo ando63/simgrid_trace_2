90dbe7aa3248a8eff52bbb85197d83a22d32542c
ando63 <126371445+ando63@users.noreply.github.com>
2024-04-15 11:40:13 +0900

    Update gen_randcs.py

--- NPB3.3-MPI/cs_edges/gen_randcs.py
+++ NPB3.3-MPI/cs_edges/gen_randcs.py
@@ -12,9 +12,40 @@ def generate_random_perfect_matching(graph, seed):
     matching_graph.add_edges_from(matching_edges)
     return matching_graph
 
+def generate_random_IO_matching(gragh, seed):
+    rs_1 = np.random.RandomState(seed)
+    rs_2 = np.random.RandomState(seed + 1000)
+    nodes_1 = list(gragh.nodes)
+    nodes_2 = list(gragh.nodes)
+    rs_1.shuffle(nodes_1)
+    rs_2.shuffle(nodes_2)
+    flag = False
+    while flag:
+        for i in range(0, len(nodes_1)):
+            if (nodes_1[i] == nodes_2[i]):
+                if (i+1 == len(node_1))
+                    swap = nodes_2[i] 
+                    nodes_2[i] = nodes_2[0]
+                    nodes_2[0] = swap
+                    i=0
+                    break
+                else:
+                    swap = nodes_2[i]
+                    nodes_2[i] = nodes_2[i+1]
+                    nodes_2[i+1] = swap
+                    i=0
+                    break
+            flag = True
+    
+    matching_edges = [(nodes_1[i], nodes_2[i]) for i in range(0, len(nodes_1))]
+    matching_gragh = nx.Gragh()
+    matching_gragh.add_edges_from(matching_edges)
+    return matching_gragh
+
 def gen_randcs(n_nodes, seed):
     complete_graph = nx.complete_graph(n_nodes)
-    random_matching = generate_random_perfect_matching(complete_graph, seed)
+    #random_matching = generate_random_perfect_matching(complete_graph, seed)
+    random_matching = generate_random_IO_matching(complete_graph, seed)
     
     return random_matching
 
@@ -28,4 +59,5 @@ if __name__ == "__main__":
     
     G_randcs = gen_randcs(node_size, seed)
     print(G_randcs.edges)
-    nx.write_edgelist(G_randcs, "randcs_{}_{}.edges".format(node_size, seed), data=False)
+    #nx.write_edgelist(G_randcs, "randcs_{}_{}.edges".format(node_size, seed), data=False)
+    nx.write_edgelist(G_randcs, "randcs_IO_{}_{}.edges".format(node_size, seed), data=False)
