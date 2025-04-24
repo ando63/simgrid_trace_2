4fe29de8ca65b27f9288c0abdca37cb580211079
ando63 <126371445+ando63@users.noreply.github.com>
2025-04-24 10:20:20 +0900

    Update NPB_multi_exec.py

--- NPB3.3-MPI/NPB_multi_exec.py
+++ NPB3.3-MPI/NPB_multi_exec.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
     for i in range(10):
       smpirun_single(basename, app, app_size)
-      log_file = 'simgrid_topo/tree_8_cg.A.8.log'  # ログファイルのパスを指定
+      log_file = 'simgrid_topo/tree_8_8_cg.A.8.log'  # ログファイルのパスを指定
       time, mops = extract_metrics_from_log(log_file)
       time_cg_10 += float(time) /10
       mops_cg_10 += float(mops) /10
