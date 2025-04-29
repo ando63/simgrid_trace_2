7cc3c6f47b258a6e9b8ebe3b10b3a5f0b2a6f227
ando63 <126371445+ando63@users.noreply.github.com>
2025-04-29 11:09:08 +0900

    Update xmlgen_64_poweredtree.py

--- NPB3.3-MPI/xmlgen_64_poweredtree.py
+++ NPB3.3-MPI/xmlgen_64_poweredtree.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     npr = args.integer
     
     out_dir = "simgrid_topo"
-    edge = "tree_64"
+    edge = "poweredtree_64"
     
     # print(header)
     # print(link_node_router.format(0, 1, 2))
